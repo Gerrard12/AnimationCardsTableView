287149bb690fdcfecba34edbe1c87cba525cdca7
Gerardo <gsanchezc1211@gmail.com>
2018-02-22 12:18:11 -0500

    TableView like cards

---
+++ cards/CardsCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardsCell.swift
+//  cards
+//
+//  Created by gerardo on 21/02/18.
+//  Copyright © 2018 Orbis. All rights reserved.
+//
+
+import UIKit
+
+class CardsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
